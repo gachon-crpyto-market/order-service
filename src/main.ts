16679041e60689f671a33bfc2015e13bf0c98370
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-10-30 03:22:34 +0900

    Feat: DTO 관련 ValidationPipe 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 없는 속성값은 제거하고 에러메시지 출력
+      transform: true, // DTO에 정의된 타입으로 변환
+      forbidNonWhitelisted: true, // DTO에 없는 속성값이 들어오면 에러메시지 출력
+    }),
+  );
   await app.listen(3000);
 }
+
 bootstrap();
