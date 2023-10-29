db869fb02a3850189c4aa1c67509b64cc2d5a052
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-10-30 03:25:57 +0900

    Feat: 매수 주문, 매도 주문, 매도 DB 데이터 가져오기 추가

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { OrderService } from './order.service';
+import { OrderDto } from './dto/order.dto';
 
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
+  @Post('bid')
+  async bid(@Body() orderDto: OrderDto): Promise<number | number[]> {
+    return await this.orderService.setBidOrder(orderDto);
+  }
+
+  @Post('ask')
+  async ask(@Body() orderDto: OrderDto): Promise<number | number[]> {
+    return await this.orderService.setAskOrder(orderDto);
+  }
+
   @Get('bidList')
   async getBidList(): Promise<any> {
     return await this.orderService.getBidList();
+  }
+
+  @Get('askList')
+  async getAskList(): Promise<any> {
+    return await this.orderService.getAskList();
   }
 }
