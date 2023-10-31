8aada95fac9b1bbe04b3f149f69afb608285f7ac
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-10-31 22:19:14 +0900

    Feat: 웹소켓용 events 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
 import { OrderModule } from './order/order.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [RedisModule, OrderModule],
+  imports: [RedisModule, OrderModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
