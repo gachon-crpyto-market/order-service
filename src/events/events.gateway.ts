eef6dca5cf3350cd69dcd2b6671469d0e2af8286
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-11-02 02:12:01 +0900

    Feat: 기본적인 연결 설정 기능 추가

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,9 +1,42 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  ConnectedSocket,
+  MessageBody,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class EventsGateway {
+@WebSocketGateway(80, {
+  namespace: 'events',
+  cors: { origin: '*' },
+})
+export class EventsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
+{
+  @WebSocketServer()
+  server: Server;
+  private readonly logger: Logger = new Logger(EventsGateway.name);
+
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
+  handleMessage(@MessageBody() body: any): string {
+    this.server.emit('message', body);
     return 'Hello world!';
+  }
+
+  afterInit() {
+    this.logger.log('웹소켓 서버 초기화 ✅');
+  }
+
+  handleConnection(@ConnectedSocket() client: Socket) {
+    this.logger.log(`Client Connected : ${client.id}`);
+  }
+
+  handleDisconnect(@ConnectedSocket() client: Socket) {
+    this.logger.log(`Client Disconnected : ${client.id}`);
   }
 }
