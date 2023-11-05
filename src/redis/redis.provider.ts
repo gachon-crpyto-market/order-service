cc5ab0bfb141ccce55c66daf635da97f68e9c9b4
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-11-05 21:52:11 +0900

    Feat: 개발/배포 환경에 따른 redis url 분리

--- src/redis/redis.provider.ts
+++ src/redis/redis.provider.ts
@@ -5,7 +5,10 @@ export const redisProvider = [
     provide: 'BID_REDIS_CLIENT',
     useFactory: async () => {
       const redisClient = createClient({
-        url: 'redis://localhost:6381',
+        url:
+          process.env.NODE_ENV === 'production'
+            ? 'redis://host.docker.internal:6381'
+            : 'redis://localhost:6381',
       });
       await redisClient.connect();
       return redisClient;
@@ -15,7 +18,10 @@ export const redisProvider = [
     provide: 'ASK_REDIS_CLIENT',
     useFactory: async () => {
       const redisClient = createClient({
-        url: 'redis://localhost:6382',
+        url:
+          process.env.NODE_ENV === 'production'
+            ? 'redis://host.docker.internal:6382'
+            : 'redis://localhost:6382',
       });
       await redisClient.connect();
       return redisClient;
